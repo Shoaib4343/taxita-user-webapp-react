ac8f71c14af8d71bd1fbd48196833d8a8e1497a7
Shoaib <mohammadshoaibkhan4343@gmail.com>
2025-08-21 15:44:41 +0500

    dashboard apis

--- src/services/dashboard.js
+++ src/services/dashboard.js
@@ -1,9 +1,12 @@
 import axiosInstance from "./axiosInstance";
 
+// Dashboard api 
 export const dashboardApi = () => {
   return axiosInstance.get("/dashboard");
 };
 
+
+// Transaction api for icome and expence
 export const createTransactionApi = (data) => {
   // Detect if it's FormData
   if (data instanceof FormData) {
@@ -13,3 +16,22 @@ export const createTransactionApi = (data) => {
   }
   return axiosInstance.post("/transactions", data);
 };
+
+
+// Weekly expenses api
+export const weeklyExpensesApi = () => {
+  return axiosInstance.get("/weekly-expenses");
+}
+
+
+
+
+
+
+
+// Weekly income api
+export const weeklyIncomeApi = () => {
+  return axiosInstance.get("/weekly-income");
+}
+
+
