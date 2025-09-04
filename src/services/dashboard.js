03e48e2cb2f6caa54e7f990eea0d42dbe8718ead
Shoaib <mohammadshoaibkhan4343@gmail.com>
2025-09-04 17:59:13 +0500

    dashboard apis

--- src/services/dashboard.js
+++ src/services/dashboard.js
@@ -56,3 +56,66 @@ export const plainsApi = () => axiosInstance.get("/plans");
 // previous-trading-years
 export const previousTradingYearApi = ()=> axiosInstance.get("/previous-trading-years");
 
+// register-trading-year
+export const registerTradingYearApi = (data) => {
+  return axiosInstance.post("/register-trading-year", data);
+};
+
+
+
+// /trading-year/activate
+export const activateTradingYearApi = (data) => {
+  return axiosInstance.post(`/trading-year/activate`, data);
+};
+
+
+
+// --------------------------- /vehicles -------------------------------------------
+
+// /vehicles
+export const vehiclesApi = (data) => {
+  return axiosInstance.post("/vehicles", data);
+};
+
+// /dvla-enquiry
+export const dvlaEnquiryApi = (data) => {
+  return axiosInstance.post("/dvla-enquiry", data);
+};
+
+
+// vehicles get all
+export const vehiclesGetAllApi = () => {
+  return axiosInstance.get("/vehicles");
+};
+
+
+
+
+// --------------------------- Percentage Adjustment -------------------------------------------
+
+//percentage-adjustments Post Api
+export const percentageAdjustmentsPostApi = (data)=>{
+  return axiosInstance.post("/percentage-adjustments", data); 
+};
+
+
+// /percentage-adjustments/2 Get Api
+export const percentageAdjustmentsGetApi = (id)=>{
+  return axiosInstance.get(`/percentage-adjustments/${id}`); 
+};
+
+
+
+// /percentage-adjustments/2 Put Api
+export const percentageAdjustmentsPutApi = (id, data)=>{
+  return axiosInstance.put(`/percentage-adjustments/${id}`, data);
+};
+
+
+
+// /percentage-adjustments get api all index listing
+export const percentageAdjustmentsGetAllApi = ()=>{
+  return axiosInstance.get("/percentage-adjustments");
+};
+
+
