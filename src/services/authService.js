71f8ca5d96e122395e3fb380ca7e48ea80cb7b9b
Shoaib <mohammadshoaibkhan4343@gmail.com>
2025-09-04 17:59:59 +0500

    auth api

--- src/services/authService.js
+++ src/services/authService.js
@@ -16,3 +16,15 @@ export const logoutApi  = ()=>{
 export const registerApi = (data) => {
     return axiosInstance.post("/register", data);
 }
+
+
+// forgot password
+export const forgotPasswordApi = (data) => {
+    return axiosInstance.post("/forgot-password", data);
+}
+
+
+// reset password
+export const resetPasswordApi = (data) => {
+    return axiosInstance.post("/reset-password", data);
+}
